e06d00a001d5136e3ce2f49f0ccdf7b111ad70d9
TonyChG <antoine.chiny@gmail.com>
2015-10-29 21:38:47 +0100

    ajout du placement des bateaux

--- game.js
+++ game.js
@@ -120,7 +120,7 @@ function putShips (grid) {
 }
 
 // Initialise la grille de box
-function initGridBox() 
+function initGridBox(playerGrid, computerGrid)
 {
   $('.box').each(function(index){
     if (index < 100) {
@@ -144,29 +144,136 @@ function initGridBox()
   })
 
   .bind('click', function(click) {
-    $(this).css('background-color', '#0000FF');
     x = $(this).attr('data-i') % 10;
     y = parseInt($(this).attr('data-i')/10);
     n = $(this).attr('grid-n');
     console.log('y', y, 'x', x, 'n', n);
+    if (n == 1) {
+      $(this).css('background-color', '#0000FF');
+    }
   });
 }
 
+function getBox(x,y) {
+  $('.box').each(function(index, box) {
+    if (x == (index % 10) && y == parseInt(index/10)) {
+      return box;
+    }});
+}
+
+function possibleShipPosition(css, grid, size) {
+  x = css.attr('data-i')%10;
+  y = parseInt(css.attr('data-i')/10);
+  possibleDir = getDirections(grid, x, y, 5);
+  $.each(possibleDir, function(index, key) {
+    for (i = 0; i < size; i++) {
+      if (key === "left") {
+      // getBox(x+i, y).css('background-color', '#FF0040');
+        $('.box').each(function(index, box) {
+          if (x == (index % 10)+i && y == parseInt(index/10)) {
+            $(this).css('background-color', '#FF0040');
+          }
+        });
+      } else if (key == "right") {
+        $('.box').each(function(index, box) {
+          if (x == (index % 10)-i && y == parseInt(index/10)) {
+            $(this).css('background-color', '#FF0040');
+          }
+        });
+      } else if (key == "down") {
+        $('.box').each(function(index, box) {
+          if (x == (index % 10) && y == parseInt(index/10)-i) {
+            $(this).css('background-color', '#FF0040');
+          }
+        });
+      } else if (key == "up") {
+        $('.box').each(function(index, box) {
+          if (x == (index % 10) && y == parseInt(index/10)+i) {
+            $(this).css('background-color', '#FF0040');
+          }
+        });
+      }
+   }
+  });
+}
+
+function clearGrid(grid) {
+  $('.box').each(function(index) {
+    $(this).css('background-color', '#8000FF');
+  });
+}
+
+function getDirSelected(lastY, lastX, clickY, clickX) {
+
+}
+
+function getShipSelection(grid) {
+  boatPlace = 0;  
+  lastX = [];
+  lastY = [];
+
+  if (boatPlace == 0)
+  {
+    $('.display')
+    .css('left', $(document).width()/3)
+    .css('bottom', '0%')
+    .animate({
+      bottom: '30%'
+    }, 'slow', 'linear');
+    $('.box')
+    .bind('mouseenter', function() {
+      $(this).css({opacity: 1});
+    })
+    .bind('mouseleave', function() {
+      $(this).css({opacity: 0.3});
+    })
+    .bind('click', function() {
+      x = $(this).attr('data-i') % 10;
+      y = parseInt($(this).attr('data-i')/10);
+      lastX.push(x);
+      lastY.push(y);
+      if (lastY.length > 1 && lastX.length > 1) {
+        getDirSelected(lastY[lastY.length-2], lastX[lastX.length-2], y, x);
+      }
+      clearGrid(grid);
+      possibleShipPosition($(this), grid, 5);
+    });
+  }
+  $('.box').each(function(index){
+    if (index < 100) {
+      $(this).attr('data-i', index);
+      $(this).attr('grid-n', 1)
+      $(this).css({opacity: 0.3});
+    } else {
+      $(this).attr('data-i', index-100);
+      $(this).attr('grid-n', 2)
+      $(this).css({opacity: 0.3});
+    }
+  });
+}
+
+// Change le css des box
 function displayShips(grid) {
   $('.box').each(function(index){
     if (index >= 100) {
       $(this).css('background-color', grid[parseInt((index-100)/10)][(index-100)%10].color);
     }
-  });  
+  });
 }
 
-function mainGame() 
+function mainGame()
 {
-  initGridBox();
-  playerGrid = newGrid(GRID_SIZE);
-  computerGrid = newGrid(GRID_SIZE);
-  playerGrid = putShips(playerGrid);
-  displayShips(playerGrid);
+  var boatIsPlace = false;
+
+  if (boatIsPlace) {
+    playerGrid = newGrid(GRID_SIZE);
+    initGridBox(playerGrid, computerGrid);
+    playerGrid = putShips(playerGrid);
+    displayShips(playerGrid);
+  } else {
+    computerGrid = newGrid(GRID_SIZE);
+    boatIsPlace = getShipSelection(computerGrid);
+  }
 }
 
 mainGame();
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
     <div class="container">
+        <h1 class="display">Placer le porte-avion (taille = 5 cases)</h1>
         <h1>IA</h1>
         <div class="grid1">
             <div class="line">
@@ -251,7 +252,7 @@
                 <div class="box"></div>
                 <div class="box"></div>
             </div>
-        </div>       
+        </div>
     </div>
     <script type="text/javascript" src="jquery.js"></script>
     <script type="text/javascript" src="game.js"></script>
